e34234f4aa223d09e742b4e861528bfa095b04f0
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-07-01 13:12:48 -0300

    Criada classe Xadrez e impresso o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import Xadrez.PartidaXadrez;
+
 public class Program {
 
     public static void main(String[] args) {
+
+        PartidaXadrez partidaXadrez = new PartidaXadrez();
+        UI.imprimirTabuleiro(partidaXadrez.getPecas());
 
     }
 }
